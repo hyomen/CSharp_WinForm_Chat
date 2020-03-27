fdc35c3e23bd938beb9c10a8ef616dc60ff9534d
hyomen <5656ghgh@naver.com>
2020-03-27 23:03:55 +0900

    Add files via upload

---
+++ chattingServer/chattingServer/ChatServerForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace chattingServer
+{
+    partial class ChatServerForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ClientList = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Enter = new System.Windows.Forms.Button();
+            this.ControlInput = new System.Windows.Forms.TextBox();
+            this.myConsole = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.myConsole);
+            this.groupBox1.Controls.Add(this.ClientList);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 370);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Console";
+            // 
+            // ClientList
+            // 
+            this.ClientList.FormattingEnabled = true;
+            this.ClientList.ItemHeight = 12;
+            this.ClientList.Location = new System.Drawing.Point(648, 20);
+            this.ClientList.Name = "ClientList";
+            this.ClientList.Size = new System.Drawing.Size(120, 160);
+            this.ClientList.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Enter);
+            this.groupBox2.Controls.Add(this.ControlInput);
+            this.groupBox2.Location = new System.Drawing.Point(12, 389);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(776, 49);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Control";
+            // 
+            // Enter
+            // 
+            this.Enter.Location = new System.Drawing.Point(694, 20);
+            this.Enter.Name = "Enter";
+            this.Enter.Size = new System.Drawing.Size(75, 23);
+            this.Enter.TabIndex = 1;
+            this.Enter.Text = "Enter";
+            this.Enter.UseVisualStyleBackColor = true;
+            this.Enter.Click += new System.EventHandler(this.ControlEnter);
+            // 
+            // ControlInput
+            // 
+            this.ControlInput.Location = new System.Drawing.Point(7, 21);
+            this.ControlInput.Name = "ControlInput";
+            this.ControlInput.Size = new System.Drawing.Size(680, 21);
+            this.ControlInput.TabIndex = 0;
+            this.ControlInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ControlInput_KeyDown);
+            // 
+            // myConsole
+            // 
+            this.myConsole.Location = new System.Drawing.Point(7, 20);
+            this.myConsole.Name = "myConsole";
+            this.myConsole.ReadOnly = true;
+            this.myConsole.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.myConsole.Size = new System.Drawing.Size(635, 344);
+            this.myConsole.TabIndex = 2;
+            this.myConsole.Text = "";
+            // 
+            // ChatServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ChatServerForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ChatServerForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button Enter;
+        private System.Windows.Forms.TextBox ControlInput;
+        private System.Windows.Forms.ListBox ClientList;
+        private System.Windows.Forms.RichTextBox myConsole;
+    }
+}
+
---
+++ chattingServer/chattingServer/ChatServerForm.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace chattingServer
+{
+    public partial class ChatServerForm : Form
+    {
+        public string bufferedList;
+        public List<string> users = new List<string>();
+        TcpListener server;
+        public void AddText(string text)
+        {
+            myConsole.AppendText(text);
+            myConsole.Select(myConsole.Text.Length, 0);
+            myConsole.ScrollToCaret();
+        }
+        void ControlEnter(object sender, EventArgs e)
+        {
+            string input = ControlInput.Text;
+            Controller(input);
+            ControlInput.Text = string.Empty;
+            ControlInput.Focus();
+        }
+        void RefreshChatters()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            while (true)
+            {
+                bufferedList = "**userlist** ";
+                ClientList.Items.Clear();   
+                foreach(string user in users)
+                {
+                    ClientList.Items.Add(user);
+                    bufferedList += (user+" ");
+                }
+                Thread.Sleep(1000);
+            }
+        }
+        void OpenServer(object s)
+        {
+            string serverIP = s.ToString().Substring("/open ".Length);
+            TcpClient client = new TcpClient();
+            const int Port = 10203;
+            IPEndPoint serverAddr = new IPEndPoint(IPAddress.Parse(serverIP), Port);
+            server = new TcpListener(serverAddr);
+            try
+            {
+                server.Start();
+            }
+            catch (SocketException e)
+            {
+                MessageBox.Show("올바르지 않은 주소입니다.");
+                return;
+            }
+            AddText(String.Format("Server Opened. [{0}]\r\n", serverAddr.ToString()));
+            while (true)
+            {
+                try
+                {
+                    client = server.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+                    byte[] buffer = new byte[1024];
+                    int bytes = stream.Read(buffer, 0, buffer.Length);
+                    string userID = Encoding.Default.GetString(buffer, 0, bytes);
+                    users.Add(userID);
+                    MyChatServer mychat = new MyChatServer(client, userID, this);
+                    Thread serverThread = new Thread(new ThreadStart(mychat.Listen));
+                    serverThread.IsBackground = true;
+                    serverThread.Start();
+                }
+                catch (Exception e) { break; }
+            }
+            client.Close();
+            server.Stop();
+        }
+        void CloseServer()
+        {
+            // 서버 닫는 처리
+        }
+        void Controller(string s)
+        {
+            if (s.Equals(string.Empty)) return;
+            else if(s.Equals("/close"))
+            {
+                CloseServer();
+                AddText("[Server] Closed.\r\n");
+            }
+            else if (s.StartsWith("/open "))
+            {
+                Thread open = new Thread(OpenServer);
+                open.IsBackground = true;
+                open.Start(s);
+            }
+        }
+        public ChatServerForm()
+        {
+            InitializeComponent();
+        }
+        private void ControlInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return) ControlEnter(sender, e);
+        }
+
+        private void ChatServerForm_Load(object sender, EventArgs e)
+        {
+            Thread refresher = new Thread(RefreshChatters);
+            refresher.IsBackground = true;
+            refresher.Start(); 
+        }
+    }
+}
+
+
+/*
+ * 
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace chattingServer
+{
+    public partial class ChatServerForm : Form
+    {
+        public string bufferedList;
+        public List<string> spams = new List<string>();
+        public List<string> bans = new List<string>();
+        public List<string> users = new List<string>();
+        TcpListener server;
+        public void AddText(string text)
+        {
+            myConsole.AppendText(text);
+            myConsole.Select(myConsole.Text.Length, 0);
+            myConsole.ScrollToCaret();
+        }
+        void ControlEnter(object sender, EventArgs e)
+        {
+            string input = ControlInput.Text;
+            Controller(input);
+            ControlInput.Text = string.Empty;
+            ControlInput.Focus();
+        }
+        void RefreshChatters()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            while (true)
+            {
+                bufferedList = "**userlist** ";
+                ClientList.Items.Clear();   
+                foreach(string user in users)
+                {
+                    ClientList.Items.Add(user);
+                    bufferedList += (user+" ");
+                }
+                Thread.Sleep(1000);
+            }
+        }
+        void OpenServer(object s)
+        {
+            string serverIP = s.ToString().Substring("/open ".Length);
+            TcpClient client = new TcpClient();
+            const int Port = 10203;
+            IPEndPoint serverAddr = new IPEndPoint(IPAddress.Parse(serverIP), Port);
+            server = new TcpListener(serverAddr);
+            try
+            {
+                server.Start();
+            }
+            catch(SocketException e)
+            {
+                MessageBox.Show("올바르지 않은 주소입니다.");
+                return;
+            }
+            AddText(String.Format("Server Opened. [{0}]\r\n", serverAddr.ToString()));
+            while (true)
+            {
+                try
+                {
+                    client = server.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+                    byte[] buffer = new byte[1024];
+                    int bytes = stream.Read(buffer, 0, buffer.Length);
+                    string userID = Encoding.Default.GetString(buffer, 0, bytes);
+                    users.Add(userID);
+                    MyChatServer mychat = new MyChatServer(client, userID, this);
+                    Thread serverThread = new Thread(new ThreadStart(mychat.Listen));
+                    serverThread.IsBackground = true;
+                    serverThread.Start();
+                }
+                catch (Exception e) { break; }
+            }
+            client.Close();
+            server.Stop();
+        }
+        void CloseServer()
+        {
+            // 서버 닫는 처리
+        }
+        void Controller(string s)
+        {
+            if (s.Equals(string.Empty)) return;
+            else if(s.Equals("/close"))
+            {
+                CloseServer();
+                AddText("[Server] Closed.\r\n");
+            }
+            else if (s.StartsWith("/open "))
+            {
+                Thread open = new Thread(OpenServer);
+                open.IsBackground = true;
+                open.Start(s);
+            }
+            else if (s.Equals("/spamlist"))
+            {
+                AddText("금지어 목록은 아래와 같습니다.\r\n");
+                foreach (string spam in spams) AddText(spam + "\r\n");
+                AddText("=======================\r\n");
+            }
+            else if (s.StartsWith("/spam "))
+            {
+                string spam = s.Substring("/spam ".Length);
+                if (spams.Contains(spam))
+                {
+                    AddText(string.Format("[Server] Spam {0} is already banned.\r\n", spam));
+                }
+                else
+                {
+                    spams.Add(spam);
+                    AddText(string.Format("[Server] Spam {0} Added.\r\n", spam));
+                }
+            }
+            else if (s.StartsWith("/unspam "))
+            {
+                string unspam = s.Substring("/unspam ".Length);
+                if (spams.Remove(unspam))
+                {
+                    AddText(string.Format("[Server] Spam {0} deleted.\r\n", unspam));
+                }
+                else
+                {
+                    AddText(string.Format("[Server] {0} is not banned.\r\n", unspam));
+                }
+            }
+            else if (s.StartsWith("/ban "))
+            {
+                string ID = s.Substring("/ban ".Length);
+                bans.Add(ID);
+                AddText(string.Format("[Server] Ban : {0} \r\n", ID));
+            }
+        }
+        public ChatServerForm()
+        {
+            InitializeComponent();
+        }
+        private void ControlInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return) ControlEnter(sender, e);
+        }
+
+        private void ChatServerForm_Load(object sender, EventArgs e)
+        {
+            Thread refresher = new Thread(RefreshChatters);
+            refresher.IsBackground = true;
+            refresher.Start(); 
+        }
+    }
+}
+
+ *
+ * 
+ * */
